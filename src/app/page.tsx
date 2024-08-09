ebaabfe7d7a8ae7b6bab5e9dd03992ed319e4b4d
IbnuJabir <kedirjabir12@gmail.com>
2024-08-09 12:11:43 +0300

    expandable grid cards are removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
         <Programs />
         <Highlights />
         <Offers />
-        <ExpandableCardDemo />
+        {/* <ExpandableCardDemo /> */}
         <Contact  />
         <Footer />
       </div>
