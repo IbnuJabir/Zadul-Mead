31a19637bd78016aa6b769cc8f4e2d8afdf0ea2d
IbnuJabir <kedirjabir12@gmail.com>
2024-08-14 11:42:48 +0300

    desktop version footer component completed

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,46 +1,57 @@
 import React from "react";
+import { AiFillYoutube } from "react-icons/ai";
+import { BsTelegram } from "react-icons/bs";
+import { FaFacebook, FaWhatsapp } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div className="w-full bg-contact-bkg bg-no-repeat bg-cover bg-center bg-slate-700 py-4">
+    <div className="w-full bg-contact-bkg  bg-no-repeat bg-cover bg-center py-4">
       <h1 className="text-white font bold text-3xl m-4">Contact Us</h1>
-      <div className="w-[ful] h-full flex flex-col gap-4 md:flex-row md:justify-around font-montserrat mt-10">
-        <div className="w-[90%] md:w-[35%] h-96 m-auto bg-slate-300 bg-opacity-30 flex flex-col gap-4 p-4 rounded-lg">
-          <p className="text-xl text-center text-white"> Drop us a message</p>
-          <input
-            type="text"
-            placeholder="Full Name"
-            className="rounded-[5px] p-1 pl-4"
-          />
-          <div className="flex justify-between w-full">
+      <div className="w-full flex flex-col gap-4 md:flex-row md:justify-around md:items-top  font-montserrat mt-10 md:py">
+        <div className="w-[90%] md:w-[50%] flex flex-col items-center justify- mx-auto">
+          <div className="w-full md:w-[70%] h-96 mx-auto bg-slate-300 bg-opacity-30 flex flex-col gap-4 p-4 rounded-lg">
+            <p className="text-xl text-center text-white"> Drop us a message</p>
             <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-              className="w-[45%] rounded-[5px] p-1 pl-4"
+              type="text"
+              placeholder="Full Name"
+              className="rounded-[5px] p-1 pl-4"
             />
-            <input
-              type="number"
-              name="phone"
-              id="phone"
-              placeholder="Phone"
-              className="w-[45%] rounded-[5px] border-none p-1 pl-4"
-            />
+            <div className="flex justify-between w-full">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                className="w-[45%] rounded-[5px] p-1 pl-4"
+              />
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                placeholder="Phone"
+                className="w-[45%] rounded-[5px] border-none p-1 pl-4"
+              />
+            </div>
+            <textarea
+              name="message"
+              id="message"
+              cols={30}
+              rows={6}
+              placeholder=" Your message"
+              className="rounded-[5px] pl-4"
+            ></textarea>
+            <button className="w-full px-4 py-2 rounded-[5px] border-none flex text-center items-center justify-center bg-white mt-4">
+              Send Message
+            </button>
           </div>
-          <textarea
-            name="message"
-            id="message"
-            cols={30}
-            rows={6}
-            placeholder=" Your message"
-            className="rounded-[5px] pl-4"
-          ></textarea>
-          <button className="w-full px-4 py-2 rounded-[5px] border-none flex text-center items-center justify-center bg-white mt-4">
-            Send Message
-          </button>
+          <div className="hidden md:flex md:gap-4 md:justify-center text-white py-10">
+            <BsTelegram className="size-[35px] lg:size-[40px]" />
+            <FaFacebook className="size-[35px] lg:size-[40px]" />
+            <AiFillYoutube className="size-[35px] lg:size-[40px]" />
+            <FaWhatsapp className="size-[35px] lg:size-[40px]" />
+          </div>
         </div>
-        <div className="w-[90%] h-96 md:w-[55%] bg-slate-400 m-auto rounded-xl">
+        <div className="w-[90%] h-[28.5rem] md:w-[65%] mx-auto rounded-xl md:pr-14">
           {/* Map */}
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d985.3138038617237!2d38.68982031586916!3d8.948614305259683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b80e159e07527%3A0x8029ecd6cbe2cded!2zTWFzamlkIEFsLUJhZHIg4Ymg4Yu14YitIOGImOGIteGMheGLtSDZhdiz2KzYrw!5e0!3m2!1sen!2set!4v1722911142213!5m2!1sen!2set"
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,15 +5,41 @@ import { FaWhatsapp } from "react-icons/fa";
 import { AiFillYoutube } from "react-icons/ai";
 function Footer() {
   return (
-    <div className="w-full h-24 bg-slate-900 flex items-center justify-around">
-      <p className="text-white font-montserrat">Zadul Mead</p>
-      <div className="flex gap-4 text-white">
-        <BsTelegram className="size-[20px]" />
-        <FaFacebook className="size-[20px]" />
-        <AiFillYoutube className="size-[20px]" />
-        <FaWhatsapp className="size-[20px]" />
+    <>
+      <div className="w-full h-24 bg-slate-900 md:hidden flex items-center justify-around font-montserrat">
+        <p className="text-white font-montserrat">Zadul Mead</p>
+        <div className="flex gap-4 text-white">
+          <BsTelegram className="size-[20px]" />
+          <FaFacebook className="size-[20px]" />
+          <AiFillYoutube className="size-[20px]" />
+          <FaWhatsapp className="size-[20px]" />
+        </div>
       </div>
-    </div>
+      <div className="hidden w-full h-56 bg-slate-900 md:flex md:items-center md:justify-between font-montserrat px-20">
+        <div className="flex flex-col w-[30%] font-montserrat">
+          <p className="text-white font-montserrat text-[28px]">Zadul Mead</p>
+          <p className="text-gray-400 text-[10px] ">
+            With lots of unique blocks, you can easily build a page without
+            coding. Build your next landing page.
+          </p>
+        </div>
+        <div className="flex justify-between text-white w-1/3">
+          <div className="flex flex-col gap-2">
+            <p className="text-gray-400 text-[12px]">Zad Al-Mead</p>
+            <a className="text-[13px]">Home</a>
+            <a className="text-[13px]">About Us</a>
+            <a className="text-[13px]">Announcements</a>
+            <a className="text-[13px]">Register</a>
+            <a className="text-[13px]">Contact</a>
+          </div>
+          <div className="flex flex-col gap-2">
+          <p className="text-gray-400 text-[12px]">Services</p>
+            <a className="text-[13px]">Mesjid</a>
+            <a className="text-[13px]">Madrasa</a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
