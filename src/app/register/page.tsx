89f56fb5179301fd713ebf0e28fce53a6793a3f7
IbnuJabir <kedirjabir12@gmail.com>
2024-08-24 00:43:11 +0300

    use new back ground image

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Register() {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full h-screen bg-old_mosque bg-no-repeat bg-center bg-cover font-cinzel'>
         <Navbar />
     <h1>Register</h1>
     </div>
