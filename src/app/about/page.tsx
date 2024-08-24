a29af57964680b41ac48bae8a5f5b041e58399f2
IbnuJabir <kedirjabir12@gmail.com>
2024-08-24 23:29:50 +0300

    about page completed for desktop size

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import bkg from "../../../public/assets/about_banner.png";
 import zad from "../../../public/assets/history_of_zad.png";
 import badr from "../../../public/assets/history_of_BadrMosque.png";
+import Footer from "@/components/Footer";
 
 function About() {
   return (
@@ -29,11 +30,27 @@ function About() {
           </p>
         </div>
       </div>
-      <div className=" w-full bg-about_page2 bg-no-repeat bg-center bg-cover bg-black bg-opacity-90  flex flex-col gap-40 -z-14 py-16">
-        <div className="w-[80%]">
+      <div className=" w-full bg-about_page2 bg-no-repeat bg-center bg-cover bg-black bg-opacity-80  flex flex-col gap-20 -z-14 py-16">
+        <div className="w-[70%] ml-[10%]">
           {/* <div className=" flex flex-col gap-4"> */}
-          <h1 className="font-bold text-2xl ">From The beginning </h1>
-          <p className="font-montserrat">
+          <h1 className="font-bold text-2xl mb-4">From The beginning </h1>
+          <p className="font-montserrat text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <Image
+            src={zad}
+            alt="history-background"
+            priority
+            placeholder="blur"
+            quality={100}
+            className="w-1/3 h-1/3 float-right ml-4 mb-2"
+          />
+          <p className="font-montserrat text-justify mt-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -42,7 +59,7 @@ function About() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className="font-montserrat">
+          <p className="font-montserrat text-justify mt-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -52,49 +69,51 @@ function About() {
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
           {/* </div> */}
-          <div className="float-right">
-            <Image
-              src={zad}
-              alt="history-background"
-              priority
-              placeholder="blur"
-              quality={100}
-              className="w-40 h-44"
-            />
-          </div>
+          {/* <div className="float-right"> */}
+
+          {/* </div> */}
         </div>
-        <div className="w-full flex items-start justify-center gap-x-10">
-          <div className="w-1/2 flex flex-col gap-4">
-            <h1 className="font-bold text-2xl ">From The beginning </h1>
-            <p className="font-montserrat">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <p className="font-montserrat">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
+        <div className="w-[70%] ml-[10%]">
+          {/* <div className=" flex flex-col gap-4"> */}
+          <h1 className="font-bold text-2xl mb-4">From The beginning </h1>
+          <p className="font-montserrat text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
           <Image
             src={badr}
             alt="history-background"
             priority
             placeholder="blur"
             quality={100}
-            className="w-1/4 h-1/3"
+            className="w-1/3 h-1/3 float-left mr-4 mt-2"
           />
+          <p className="font-montserrat text-justify mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="font-montserrat text-justify mt-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          {/* </div> */}
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
