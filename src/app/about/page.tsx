9b0d415a382d3b82e7659bf22bd0941e5ed2d5cf
Yusuf <usufee42@Gmail.com>
2024-09-01 22:15:18 +0300

    mobile responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 function About() {
   return (
     <div className="w-full text-white ">
-      <div className="w-full py-10 md:py-0 md:h-[55vh] relative bg-black  bg-no-repeat bg-center bg-cover font-cinzel z-10 text-white">
+      <div className="w-full h-[55vh] relative bg-black  bg-no-repeat bg-center bg-cover font-cinzel z-10 text-white">
         <Navbar />
         <Image
           src={bkg}
@@ -20,23 +20,11 @@ function About() {
           className="object-cover align-middle md:align-top -z-10"
         />
         <div className="mt-12 ml-3 md:ml-16 w-[96%] md:w-1/2 flex flex-col  gap-3 md:gap-2">
-          <p
-           data-aos="zoom-in" 
-           data-aos-duration="1000" 
-            data-aos-once="true"
-           className="text-4xl font-extralight">About</p>
-          <h1
-           data-aos="zoom-in" 
-           data-aos-duration="1000" 
-            data-aos-once="true"
-           className="text-2xl md:text-3xl">
+          <p className="text-4xl font-extralight">About</p>
+          <h1 className=" text-2xl md:text-4xl">
             <span className="font-bold">ZAD AL-MEAD</span> Islamic Center
           </h1>
-          <p
-          data-aos="fade-up" 
-          data-aos-duration="1000" 
-         data-aos-once="true" 
-           className="font-montserrat text-[18px]">
+          <p className="font-montserrat text-[18px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
@@ -45,16 +33,8 @@ function About() {
       <div className=" w-full bg-about_page2 bg-no-repeat bg-center bg-cover bg-black bg-opacity-90  flex flex-col gap-20 -z-14 py-16">
         <div className="w-[90%] md:w-[70%] ml-[5%] md:ml-[10%] mt-10">
           {/* <div className=" flex flex-col gap-4"> */}
-          <h1 data-aos="fade-up" 
-           data-aos-duration="1000" 
-            data-aos-once="true"
-             className="font-bold text-2xl mb-4">From The beginning </h1>
-          <p 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="100"
-           data-aos-once="true"
-            className="hidden md:block font-montserrat text-justify">
+          <h1 className="font-bold text-2xl mb-4">From The beginning </h1>
+          <p className="hidden md:block font-montserrat text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -62,12 +42,7 @@ function About() {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-          <p 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="100"
-           data-aos-once="true"
-            className=" md:hidden font-montserrat text-justify">
+          <p className=" md:hidden font-montserrat text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna
           </p>
@@ -78,28 +53,14 @@ function About() {
             placeholder="blur"
             quality={100}
             className=" w-1/2 md:w-1/3 h-[270px] md:h-1/3 float-right ml-4 mb-2 "
-            data-aos="zoom-in" 
-           data-aos-duration="1000" 
-           data-aos-delay="200"
-            data-aos-once="true"
           />
-          <p
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="100"
-           data-aos-once="true"
-            className=" font-montserrat text-justify">
+          <p className=" font-montserrat text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco 
             
           </p>
-          <p 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="100"
-           data-aos-once="true"
-            className="hidden md:block font-montserrat text-justify mt-5">
+          <p className="hidden md:block font-montserrat text-justify mt-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -115,17 +76,8 @@ function About() {
         </div>
         <div className=" w-[90%] md:w-[70%] ml-[5%] md:ml-[10%] ">
           {/* <div className=" flex flex-col gap-4"> */}
-          <h1
-          data-aos="fade-up" 
-          data-aos-duration="1000" 
-           data-aos-once="true"
-            className="font-bold text-2xl mb-4">From The beginning </h1>
-          <p
-           data-aos="zoom-in" 
-           data-aos-duration="1000" 
-           data-aos-delay="100"
-            data-aos-once="true"
-             className="hidden md:block  font-montserrat text-justify">
+          <h1 className="font-bold text-2xl mb-4">From The beginning </h1>
+          <p className="hidden md:block  font-montserrat text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -133,12 +85,7 @@ function About() {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-          <p 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="300"
-           data-aos-once="true"
-            className=" md:hidden font-montserrat text-justify">
+          <p className=" md:hidden font-montserrat text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna
           </p>
@@ -149,17 +96,8 @@ function About() {
             placeholder="blur"
             quality={100}
             className="w-1/2 md:w-1/3 h-[270px] md:h-1/3 float-left mr-4 mt-2 "
-            data-aos="zoom-in" 
-           data-aos-duration="1000" 
-           data-aos-delay="200"
-            data-aos-once="true"
           />
-          <p 
-           data-aos="zoom-in" 
-           data-aos-duration="1000" 
-           data-aos-delay="100"
-            data-aos-once="true"
-             className="font-montserrat text-justify mt-2">
+          <p className="font-montserrat text-justify mt-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -168,12 +106,7 @@ function About() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-          data-aos-delay="100"
-           data-aos-once="true"
-            className="hidden md:block  font-montserrat text-justify mt-5">
+          <p className="hidden md:block  font-montserrat text-justify mt-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
--- src/app/announcemets/page.tsx
+++ src/app/announcemets/page.tsx
@@ -2,14 +2,13 @@
 import Image from "next/image";
 import React from "react";
 import bkg from "../../../public/assets/announcements_banner.png";
-import kitab from "../../../public/assets/kitab.png";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 function Announcements() {
   return (
     <div className="w-full text-white ">
-      <div className="w-full py-10 md:py-0 md:h-[55vh] relative bg-black  bg-no-repeat bg-center bg-cover font-cinzel z-10 text-white">
+      <div className="w-full h-[55vh] relative bg-black  bg-no-repeat bg-center bg-cover font-cinzel z-10 text-white">
         <Navbar />
         <Image
           src={bkg}
@@ -20,23 +19,11 @@ function Announcements() {
           className="object-cover align-center -z-10"
         />
         <div className="mt-12 ml-6 md:ml-16 w-[95%] md:w-1/2 flex flex-col gap-2">
-          <p 
-           data-aos="zoom-in" 
-           data-aos-duration="1000" 
-            data-aos-once="true"
-            className="text-2xl md:text-4xl font-extralight">Announcements of</p>
-          <h1 
-          data-aos="zoom-in" 
-          data-aos-duration="1000" 
-           data-aos-once="true"
-            className=" text-2xl md:text-3xl">
+          <p className="text-2xl md:text-4xl font-extralight">Announcements of</p>
+          <h1 className="text-4xl">
             <span className="font-bold">ZAD AL-MEAD</span> Islamic Center
           </h1>
-          <p 
-          data-aos="fade-up" 
-          data-aos-duration="1000" 
-           data-aos-once="true"
-            className="font-montserrat text-[18px]">
+          <p className="font-montserrat text-[17px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
@@ -56,23 +43,14 @@ function Announcements() {
             <div className="flex items-center justify-center flex-col md:flex-row md:justify-around">
               {/* <img src='/assets/kitab.png' alt="kitab" className="w-4/5 " /> */}
               <Image
-                data-aos="fade-right" 
-                data-aos-duration="1000" 
-                data-aos-delay="300"
-                 data-aos-once="true"
-                src={kitab}
+                src={bkg}
                 alt="kitab"
-                className="w-[90%] md:w-[321px] h-[350px] md:h-[219px] cursor-pointer md:border-4 md:border-white rounded-lg my-5"
+                className="w-[60%] md:w-[321px] h-[219px] cursor-pointer border-4 border-white rounded-md my-5"
                 placeholder="blur"
                 priority
                 onClick={() => console.log("Language")}
               />
-              <div 
-              data-aos="fade-left" 
-              data-aos-duration="1000" 
-              data-aos-delay="300"
-               data-aos-once="true"
-                className="flex flex-col gap-6 items-start">
+              <div className="flex flex-col gap-6 items-start">
                 <div className="w-full font-montserrat px-4 flex items-top justify-left gap-4">
                   <p className="w-8 h-6 text-black p-4 bg-slate-200 rounded-full flex items-center justify-center">
                     1
@@ -126,23 +104,14 @@ function Announcements() {
             <div className="flex items-center justify-center flex-col md:flex-row md:justify-around">
               {/* <img src='/assets/kitab.png' alt="kitab" className="w-4/5 " /> */}
               <Image
-               data-aos="fade-right" 
-               data-aos-duration="1000" 
-               data-aos-delay="300"
-                data-aos-once="true"
-                src={kitab}
+                src={bkg}
                 alt="kitab"
-                className="w-[90%] md:w-[321px] h-[350px] md:h-[219px] cursor-pointer md:border-4 md:border-white rounded-md my-2"
+                className="w-[60%] md:w-[321px] h-[219px] cursor-pointer border-4 border-white rounded-md my-2"
                 placeholder="blur"
                 priority
                 onClick={() => console.log("Language")}
               />
-              <div 
-              data-aos="fade-left" 
-              data-aos-duration="1000" 
-              data-aos-delay="300"
-               data-aos-once="true"
-                className="flex flex-col gap-6 items-start">
+              <div className="flex flex-col gap-6 items-start">
                 <div className="w-full font-montserrat px-4 flex items-top justify-left gap-4">
                   <p className="w-8 h-6 text-black p-4 bg-slate-200 rounded-full flex items-center justify-center">
                     1
@@ -196,23 +165,14 @@ function Announcements() {
             <div className="flex items-center justify-center flex-col md:flex-row md:justify-around">
               {/* <img src='/assets/kitab.png' alt="kitab" className="w-4/5 " /> */}
               <Image
-               data-aos="fade-right" 
-               data-aos-duration="1000" 
-               data-aos-delay="300"
-                data-aos-once="true"
-                src={kitab}
+                src={bkg}
                 alt="kitab"
-                className="w-[90%] md:w-[321px] h-[350px] md:h-[219px] cursor-pointer md:border-4 md:border-white rounded-md my-2"
+                className="w-[60%] md:w-[321px] h-[219px] cursor-pointer border-4 border-white rounded-md my-2"
                 placeholder="blur"
                 priority
                 onClick={() => console.log("Language")}
               />
-              <div
-              data-aos="fade-left" 
-              data-aos-duration="1000" 
-              data-aos-delay="300"
-               data-aos-once="true"
-                className="flex flex-col gap-6 items-start">
+              <div className="flex flex-col gap-6 items-start">
                 <div className="w-full font-montserrat px-4 flex items-top justify-left gap-4">
                   <p className="w-8 h-6 text-black p-4 bg-slate-200 rounded-full flex items-center justify-center">
                     1
