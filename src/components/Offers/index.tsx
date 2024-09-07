04055b7bfbaa333473014c59677a76ec7e000abe
Yusuf <usufee42@Gmail.com>
2024-09-07 21:10:58 +0300

    offer to orginal offers

--- src/components/Offers/index.tsx
+++ src/components/Offers/index.tsx
@@ -1,7 +1,12 @@
 "use-client";
-import { IconLibrary } from "@tabler/icons-react";
+
 import React from "react";
 import { IoLibrary } from "react-icons/io5";
+import { GrResources } from "react-icons/gr";
+import { GiFamilyHouse } from "react-icons/gi";
+import { IoMdFootball } from "react-icons/io";
+import { MdHouse } from "react-icons/md";
+import { ImSpoonKnife } from "react-icons/im";
 import PulsatingButton from "../ui/pulsating-button";
 function Offers() {
   return (
@@ -13,15 +18,16 @@ function Offers() {
          data-aos="zoom-out"
        
         className="h-fit flex flex-col gap-4 items-start justify-start">
+          <div className="w-full flex gap-3 items-center">
           <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+          
+            <h2 className="text-left"> Modern Library</h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            A well-equipped library with a vast collection of Islamic literature, academic texts, digital resources, and study spaces for students to conduct research and study.
+
             </p>
-          </div>
+        
         </div>
         <div
          data-aos="zoom-out"
@@ -30,15 +36,17 @@ function Offers() {
       
          
         className="h-fit flex flex-col gap-4 items-start justify-start">
-          <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+           <div className="flex  gap-3 items-center w-full">
+          <GrResources className="size-8" />
+         
+            <h2 className="text-left">Resource Center
+            </h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            Academic texts, digital resources, and study spaces for students to conduct research and study.
+
             </p>
-          </div>
+          
         </div>
         <div
           data-aos-once="true"
@@ -48,15 +56,16 @@ function Offers() {
 
 
           className="h-fit flex flex-col gap-4 items-start justify-start">
-          <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+             <div className="flex items-center gap-3 w-full">
+          <GiFamilyHouse className="size-8" />
+         
+            <h2 className="text-left">On-Campus Housing
+            </h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
             </p>
-          </div>
+          
         </div>
         <div
           data-aos-once="true"
@@ -65,15 +74,16 @@ function Offers() {
       
          
           className="h-fit flex flex-col gap-4 items-start justify-start">
-          <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+             <div className="flex items-center gap-3 w-full">
+          <IoMdFootball className="size-8" />
+         
+            <h2 className="text-left">Clean Dining Services
+            </h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
             </p>
-          </div>
+          
         </div>
         <div
           data-aos-once="true"
@@ -82,15 +92,16 @@ function Offers() {
    
          
           className="h-fit flex flex-col gap-4 items-start justify-start">
-          <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+             <div className="flex items-center gap-3 w-full">
+          <MdHouse className="size-8" />
+         
+            <h2 className="text-left">Meditation Spaces
+            </h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
             </p>
-          </div>
+          
         </div>
         <div 
           data-aos-once="true"
@@ -99,15 +110,15 @@ function Offers() {
          
 
           className="h-fit flex flex-col gap-4 items-start justify-start">
-          <IoLibrary className="size-8" />
-          <div className="flex flex-col gap-2 w-full">
-            <h2 className="text-left">Modern Library</h2>
+             <div className="flex items-center gap-3 w-full">
+          <ImSpoonKnife className="size-8" />
+         
+            <h2 className="text-left">Sports and Recreational</h2>
+            </div>
             <p className="w-full font-montserrat text-[11px] text-gray-300 text-left">
-              A well-equipped library with a vast collection of Islamic
-              literature, academic texts, digital resources, and study spaces
-              for students to conduct research and study.
+            With lots of unique blocks, you can easily build a page without coding. Build your next landing page.
             </p>
-          </div>
+        
         </div>
       </div>
       <div className="h-40 flex items-center justify-center">
